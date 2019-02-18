5d9aa2517ccc051de6c1a06b7195ded72efceef0
minakawa-daiki <minakawa.daiki.as@gmail.com>
2019-02-19 01:56:47 +0900

    Add create project modal

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 
-from .views import ProjectListView, ProjectCreateView, create_account
+from .views import ProjectListView, ProjectCreateView, create_account, get_faucet
 
 urlpatterns=[
     path('new/',ProjectCreateView.as_view(), name='project_new'),
     path('account/create',create_account, name='create_account'),
+    path('account/faucet',get_faucet, name='get_faucet'),
     path('',ProjectListView.as_view(),name='project_list'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -11,11 +11,12 @@ from django.urls import reverse_lazy
 
 class ProjectListView(ListView):
     model = Project
+    queryset = Project.objects.order_by('-id')
     template_name='project_list.html'
 
 class ProjectCreateView(CreateView):
+    http_method_names = ['post']
     model = Project
-    template_name = 'project_new.html'
     fields = ('title',)
 
 def create_account(request):
@@ -23,6 +24,16 @@ def create_account(request):
     json = response.json()
     if json['msg'] == 'succeeded':
         response_json = {"result": { "data": json['keys'], "error": False }}
+        return JsonResponse(response_json)
+    else:
+        response_json = {"result": { "data": json['msg'], "error": True }}
+        return JsonResponse(response_json)
+
+def get_faucet(request):
+    response = requests.get('http://faucet.cryptokylin.io/get_token?' + request.GET.get('name'))
+    json = response.json()
+    if json['msg'] == 'succeeded':
+        response_json = {"result": { "error": False }}
         return JsonResponse(response_json)
     else:
         response_json = {"result": { "data": json['msg'], "error": True }}
